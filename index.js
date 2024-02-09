930f068107d6ad338a0b08bd0dc60ab4e19c52cb
tlcgeyer <tamlincupido2402@gmail.com>
2024-02-09 16:16:47 +0200

    expressContent

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Greetings peasants.")
+import express from 'express'
+
+//You need to create an express app to gain access to the express method. 
+// Creating an express App
+const app = express()
+const port = +process.env.PORT || 4000
+/*You set up the router via the express App. np: patch is faster then put*/
+
+//Router
+app.get('/',(req, res)=> {
+    res.status(200).json({
+        msg: 'Ayo'
+    })
+})
+
+//* means everything
+app.all('*', (req, res)=> {
+    res.json({
+        status: 404, 
+        msg: '404 Page'
+    })
+})
+
+app.listen(port)

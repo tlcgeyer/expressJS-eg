665a1d276a0e6bffbc888d97a5c13374d1697626
Tamlin Geyer <146750294+tlcgeyer@users.noreply.github.com>
2024-02-12 10:54:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,12 @@ app.all('*', (req, res)=> {
 home work
 =======
 1. What is middleware?
+~Functions that can access & modify the request and response objects in an Express application.
 2. How can we use it in express?
+~Express.js has a middleware system that allows developers to define and manage middleware functions. These functions can perform tasks such as 
+adding functionality to an app, such as logging, authenication, and error-handling.
 3. How do we use it in our API?
+~ services that facilitate communication and data exchange between different applications, systems, or layers. Middleware plays a 
+crucial role in enhancing the functionality, security, and performance of APIs. 
 */
 app.listen(port)
